3acdd1a6d8a93c094f064b7133fee1406239f275
alexzigma <tor.trapeczia@gmail.com>
2023-10-30 20:53:19 +0300

    deploy

--- build.sh
+++ build.sh
@@ -2,17 +2,25 @@
 # exit on error
 set -o errexit
 
-echo "Installing the latest version of poetry..."
+# echo "Installing the latest version of poetry..."
 
-pip install --upgrade pip
+# pip install --upgrade pip
 
-pip install poetry==1.6.1
+# pip install poetry==1.6.1
 
-rm poetry.lock
+# rm poetry.lock
 
-poetry lock
+# poetry lock
 
 poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+export DJANGO_SUPERUSER_EMAIL=test@test.com
+export DJANGO_SUPERUSER_PASSWORD=admin
+export DJANGO_SUPERUSER_USERNAME=admin
+python manage.py createsuperuser --no-input
+
+
+python manage.py createsuperuser
