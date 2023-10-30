b61507e8b0f61f5d8c66d8ca842532387e5d87e8
alexzigma <tor.trapeczia@gmail.com>
2023-10-30 20:32:42 +0300

    deploy

--- build.sh
+++ build.sh
@@ -2,6 +2,16 @@
 # exit on error
 set -o errexit
 
+echo "Installing the latest version of poetry..."
+
+pip install --upgrade pip
+
+pip install poetry==1.6.1
+
+rm poetry.lock
+
+poetry lock
+
 poetry install
 
 python manage.py collectstatic --no-input
